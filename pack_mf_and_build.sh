943f169433206ac06065293d1c63314064c19977
Georgii Gvinepadze <fireshowel@gmail.com>
2025-06-04 18:58:47 +0400

    dbeaver/pro#5728 update pom configs

--- pack_mf_and_build.sh
+++ pack_mf_and_build.sh
@@ -4,5 +4,5 @@ cd ../target-bundles || exit
 mvn clean package
 cd ../features/com.ce.bundle.feature || exit
 mvn clean package
-cd ../repository || exit
+cd ../../repository || exit
 mvn clean package
