44c85a63873ca9e6fc1ac51e8d5c62b97d45a59f
serge-rider <serge@dbeaver.com>
2025-06-05 14:24:55 +0200

    dbeaver/pro#5728 Repo layout + fixes

--- pack_mf_and_build.sh
+++
@@ -1,8 +0,0 @@
-cd aggregate || exit
-mvn clean package
-cd ../target-bundles || exit
-mvn clean package
-cd ../features/com.ce.bundle.feature || exit
-mvn clean package
-cd ../../repository || exit
-mvn clean package
